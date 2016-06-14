23fe823941430de67cf9e4a84b3f5dfef4a147b2
Kara de la Marck <karadelamarck@gmail.com>
2016-06-14 19:49:01 +0100

    Refactor: with wider scope for handling odd strings

--- spec/romanizerSpec.js
+++ spec/romanizerSpec.js
@@ -25,6 +25,60 @@ describe('arrReduce', function() {
   });
 });
 
+describe('result', function() {
+  it('converts string of numbers into array of numbers', function() {
+    expect(result.convert('1,2')).toEqual([1, 2]);
+  });
+});
+
+describe('result2', function() {
+  it('converts string of numbers into sum', function() {
+    expect(result2.convert('1,2')).toEqual(3);
+  });
+});
+
+describe('result2', function() {
+  it('converts string of numbers into sum', function() {
+    expect(result2.convert('1,2,4,5,8')).toEqual(20);
+  });
+});
+
+describe('regex', function() {
+  it('converts string of numbers into sum, and deals with exceptions', function() {
+    expect(regex.convert('1,2\n3\n4')).toEqual(10);
+  });
+});
+
+describe('regex2', function() {
+  it('converts string of numbers into sum, and deals with MORE exceptions', function() {
+    expect(regex2.convert('1,2')).toEqual(3);
+  });
+});
+
+describe('regex2', function() {
+  it('converts string of numbers into sum, and deals with MORE exceptions', function() {
+    expect(regex2.convert('1£2')).toEqual(3);
+  });
+});
+
+describe('regex2', function() {
+  it('converts string of numbers into sum, and deals with MORE exceptions', function() {
+    expect(regex2.convert('1$1&1(1,1,%1)')).toEqual(6);
+  });
+});
+
+describe('regex2', function() {
+  it('converts string of numbers into sum, and deals with MORE exceptions', function() {
+    expect(regex2.convert('1$11&1(1,1,%1)')).toEqual(16);
+  });
+});
+
+describe('regex2', function() {
+  it('converts string of numbers into sum, and deals with MORE exceptions', function() {
+    expect(regex2.convert('1$%1&*(1,$£1,%1)')).toEqual(5);
+  });
+});
+
 describe('result1', function() {
   it('reduces string with exceptions, into sum', function() {
     expect(result1.convert('%,2\n3\n4')).toEqual(9);
@@ -36,33 +90,3 @@ describe('result1', function() {
     expect(result1.convert('%,2,\n3\n4!')).toEqual(9);
   });
 });
-
-// describe('result', function() {
-//   it('converts string of numbers into array of numbers', function() {
-//     expect(result.convert('1,2')).toEqual([1, 2]);
-//   });
-// });
-
-// describe('result2', function() {
-//   it('converts string of numbers into sum', function() {
-//     expect(result2.convert('1,2')).toEqual(3);
-//   });
-// });
-//
-// describe('result2', function() {
-//   it('converts string of numbers into sum', function() {
-//     expect(result2.convert('1,2,4,5,8')).toEqual(20);
-//   });
-// });
-
-// describe('regex', function() {
-//   it('converts string into array of strings, and deals with exceptions', function() {
-//     expect(regex.convert('1,2\n3\n4')).toEqual(10);
-//   });
-// });
-//
-// describe('regex2', function() {
-//   it('converts string with exceptions, into string of numbers', function() {
-//     expect(regex2.convert('%,2\n3\n4')).toEqual('234');
-//   });
-// });
--- src/romanizer.js
+++ src/romanizer.js
@@ -21,53 +21,52 @@ arrReduce.convert = function(arr) {
   return reduced;
 };
 
-var result1 = {};
-result1.convert = function (str) {
-  var cleanString = str.replace(/[\D]/g, "");
-  var arrayStrNums = cleanString.split('');
-  var arrayNums = arrayStrNums.map(function(e) {
-    return parseInt(e, 10);
-  });
-    var reduced = arrayNums.reduce(function(a, b) {
-    return a + b;
-  });
-  return reduced;
+var result = {};
+result.convert = function (str) {
+  return str
+    .split(',')
+    .map(Number);
 };
 
-// var result = {};
-// result.convert = function (str) {
-//   var arrayStrNums = str.split(',');
-//   var arrayNums = arrayStrNums.map(function(e) {
-//     return Number(e);
-//   });
-//   return arrayNums;
-// };
-//
-// var result2 = {};
-// result2.convert = function (str) {
-//   var arrayStrNums = str.split(',');
-//   var arrayNums = arrayStrNums.map(function(e) {
-//     return parseInt(e, 10);
-//   });
-//     var reduced = arrayNums.reduce(function(a, b) {
-//     return a + b;
-//   });
-//   return reduced;
-// };
-//
-// var regex = {};
-// regex.convert = function (str) {
-//   var arrayStrNums = str.split(/[,\n]/);
-//   var arrayNums = arrayStrNums.map(function(e) {
-//     return parseInt(e, 10);
-//   });
-//     var reduced = arrayNums.reduce(function(a, b) {
-//     return a + b;
-//   });
-//   return reduced;
-// };
-//
-// var regex2 = {};
-// regex2.convert = function (str) {
-//   return str.replace(/[\D]/g, "");
-// };
+var result2 = {};
+result2.convert = function (str) {
+  return str
+    .split(',')
+    .map(Number)
+    .reduce(function(a, b) {
+    return a + b;
+  });
+};
+
+var regex = {};
+regex.convert = function (str) {
+  return str
+    .split(/[,\n]/)
+    .map(Number)
+    .reduce(function(a, b) {
+    return a + b;
+  });
+};
+
+var regex2 = {};
+regex2.convert = function (str) {
+  return str
+    .split(/\D/)
+    .map(Number)
+    .reduce(function(a, b) {
+    return a + b;
+  });
+};
+
+
+function sum(a, b) {
+  return a + b;
+}
+
+var result1 = {};
+result1.convert = function (str) {
+  return str
+    .split(/\D/)
+    .map(Number)
+    .reduce(sum);
+};
